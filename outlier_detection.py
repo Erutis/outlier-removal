388e624f75ff79bac10d969db29b0adcff4df1bf
K Agajanian <kei@Mika.lan>
2022-07-22 10:42:25 -0400

    script outline

---
+++ outlier_detection.py
@@ -0,0 +1,33 @@
+# This script will be able to take in a pickle dataset and output the dataset with outliers removed
+
+
+# imports
+
+
+
+
+# grab pickle file
+
+
+
+# explore pickle file
+
+
+
+# set up outlier detection algorithm
+
+
+
+# run pickle dataset through it
+
+
+
+# print statements on how many outliers were detected
+
+
+
+# create new dataset without outliers
+
+
+
+# output new dataset to new pickle file
